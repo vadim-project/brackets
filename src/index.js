85f82cd61c2b51cd4d76e2db5cce5f89b16e25c6
vadim-project <vadim.rogozhnikov2002@gmail.com>
2023-02-12 21:11:33 +0300

    feat: complete JS Brackets.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  let flag = true;
+  while (flag) {
+    flag = false;
+    for (let symbol in bracketsConfig) {
+      for (let i = 0; i <= str.length; i++) {
+        if (bracketsConfig[symbol][0] === str[i] && bracketsConfig[symbol][1] === str[i + 1]) {
+          let asd = bracketsConfig[symbol][0] + bracketsConfig[symbol][1];
+          str = str.replace(asd, '');
+          
+          i = 0;
+          flag = true;
+        }
+      }
+    }
+  }
+  if (str.length === 0) return true;
+  else return false;
 }
+
